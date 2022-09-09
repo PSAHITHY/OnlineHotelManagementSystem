4f590c8e78cdc609a6feefe6c8fd188489e247b3
sahithya <sahithya.parnapalli@capgemini.com>
2022-09-09 15:21:51 +0530

    case study

---
+++ Receptionist-Service/src/main/java/com/receptionistservice/controller/ReceptionistController.java
@@ -0,0 +1,39 @@
+package com.receptionistservice.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.receptionistservice.entity.Receptionist;
+import com.receptionistservice.service.ReceptionistService;
+
+@RestController
+@RequestMapping("/receptionist")
+public class ReceptionistController 
+{
+  
+	@Autowired
+	private ReceptionistService receptionistService;
+	
+//	@PostMapping("/add")
+//	public ResponseEntity<Receptionist> registerReceptionist(@RequestBody Receptionist receptionist)
+//	{
+//		Receptionist savedReceptionist=receptionistService.addReceptionist(receptionist);
+//		return new ResponseEntity<>(savedReceptionist,HttpStatus.CREATED);
+//	}
+	
+	@GetMapping("/all")
+	public ResponseEntity<List<Receptionist>> fetchAllReceptionists()
+	{
+		List<Receptionist> receptionists=receptionistService.getAllReceptionists();
+		return new ResponseEntity<>(receptionists,HttpStatus.OK);
+	}
+	
+}
